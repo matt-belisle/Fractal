56602e3966394c7204cd8c09703881136803e61e
Matt Belisle <mbelisle@vitalimages.com>
2018-06-12 12:08:03 -0400

    Added kotlin files

---
+++ src/main/Kotlin/Complex.kt
@@ -0,0 +1,41 @@
+class Complex(real: Double = 0.0, imag: Double = 0.0){
+    private val real: Double = real
+    private val imag = imag
+
+    fun getReal(): Double = real
+    fun getImag(): Double = imag
+
+    operator fun plus(other:Complex): Complex = Complex(real+other.real,imag+other.imag)
+    operator fun minus(other:Complex): Complex = Complex(real-other.real,imag-other.imag)
+    operator fun times(other:Complex): Complex = Complex( (real*other.real-imag*other.imag), (real*other.imag + imag*other.real))
+    fun conjugate(): Complex = Complex(real, -1 * imag)
+    operator fun div(denom: Complex): Complex {
+        if(denom == Complex(0.0,0.0)){
+            throw Exception("Denominator magnitude was zero cannot divide")
+        }
+        val numerator = this * denom.conjugate()
+        val denominator = denom.multByConjugate()
+        return Complex(numerator.real / denominator, numerator.imag / denominator)
+
+    }
+    fun multByConjugate(): Double {
+        val real =(this * this.conjugate())
+        if(!real.imag.equals(0.0) ){
+            print(real.imag)
+            throw(Exception("Multiply by conjugate unsuccessful"))
+        }
+        return real.real
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if(other is Complex){
+            real == other.real && imag == other.imag
+        }else{
+            false
+        }
+    }
+
+    override fun toString(): String {
+        return "$real $imag i"
+    }
+}
